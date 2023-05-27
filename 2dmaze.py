97650088f8ef5b87f9f287cb6446d226fe360fde
Yves Abi Raad <yves.abiraad@lau.edu.lb>
2023-05-27 13:04:10 +0300

    added methods that move player and initialized game environment

--- 2dmaze.py
+++ 2dmaze.py
@@ -14,10 +14,41 @@ class Player(object):
     
     def __init__(self):
         self.rect = pygame.Rect(32, 32, 16, 16)
-    
+
+    #function of collision of player with wall
+    def collide(self,x,y):
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if x > 0:
+                    self.rect.right = wall.rect.left
+                if x < 0:
+                    self.rect.left = wall.rect.right
+                if y > 0:
+                    self.rect.bottom = wall.rect.top
+                if y < 0:
+                    self.rect.top = wall.rect.bottom
+
+    #moves player on either x axis or y axis , helper method for move()
+    def moveAxis(self,x,y):
+        self.rect.x +=x
+        self.rect.y +=y
+        self.collide(self,x,y)
+
+    def move(self,x,y):
+        if x != 0:
+            self.moveAxis(self,x,0)
+        if y != 0:
+            self.moveAxis(self,0,y)
+
+
 class Wall(object):
     def __init__(self,position):
         walls.append(self)
         self.rect = pygame.rect(position[0],position[1],16,16)
-        
+
+
+# now we initialize the environment
+os.environ["SDL_VIDEO_CENTERED"] = "1" #centers the game window on the screen
+pygame.init()  # initiliazes pygame library for use
+
 walls=[]
